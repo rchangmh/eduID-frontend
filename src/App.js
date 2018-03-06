cb87036a5d6cbc55eebed3e5213e075692a47179
Ronald <rchangmh@gmail.com>
2018-03-06 16:07:46 -0500

    set up framework

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Switch, Route, Link, Redirect } from 'react-router-dom'
+import { Segment, Menu } from 'semantic-ui-react'
+
+export default class App extends Component {
+
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import React from "react"
+import { render } from "react-dom"
+import { Switch, Route, Link, Redirect } from "react-router-dom"
+import { Segment, Menu } from "semantic-ui-react"
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
+  fontFamily: "sans-serif",
+  textAlign: "center"
+}
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <Switch>
+      <Route
+        exact
+        path="/teas"
+        render={props => <Teas submit={this.onSubmit} />}
+      />
+      <Route path="/vision" component={Vision} />
+      <Route
+        exact
+        path="/review"
+        render={props => <Review values={this.state.submitted} />}
+      />
+    </Switch>
   </div>
-);
+)
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"))
