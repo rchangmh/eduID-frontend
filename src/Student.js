f10018e867c64a75af11a272f26465268197e573
Ronald <rchangmh@gmail.com>
2018-03-08 08:46:06 -0500

    post-hackathon

--- src/Student.js
+++ src/Student.js
@@ -1,187 +1,194 @@
-import React, { Component } from "react"
-import { render } from "react-dom"
-import { Switch, Route, Link, Redirect } from "react-router-dom"
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { Switch, Route, Link, Redirect } from 'react-router-dom'
 import {
-  Input,
-  Menu,
-  Grid,
-  Segment,
-  Button,
-  Card,
-  Image,
-  Header,
-  Icon
-} from "semantic-ui-react"
-import axios from "axios"
+	Input,
+	Menu,
+	Grid,
+	Segment,
+	Button,
+	Card,
+	Image,
+	Header,
+	Icon
+} from 'semantic-ui-react'
+import axios from 'axios'
 
 export default class Student extends Component {
-  state = {
-    activeItem: "Profile",
-    created: true,
-    menuItems: ["Profile", "Scores", "Colleges"],
-    scores: {
-      GPA: "86.6",
-      SAT: "1300"
-    },
-    colleges: {
-      UMaryland: {
-        name: "University of Maryland",
-        image:
-          "https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/University_of_Maryland_seal.svg/1200px-University_of_Maryland_seal.svg.png",
-        status: "Not Submitted"
-      },
-      Cornell: {
-        name: "Cornell University",
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Cornell_University_seal.svg/1200px-Cornell_University_seal.svg.png",
-        status: "Not Submitted"
-      },
-      CMU: {
-        name: "Carnegie Mellon University",
-        image:
-          "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Carnegie_Mellon_University_seal.svg/1200px-Carnegie_Mellon_University_seal.svg.png",
-        status: "Not Submitted"
-      }
-    },
-    fields: { Name: "", Address: "", Email: "", DOB: "" }
-  }
+	state = {
+		activeItem: 'Profile',
+		created: true,
+		menuItems: ['Profile', 'Scores', 'Colleges'],
+		scores: {
+			GPA: '86.6',
+			SAT: '1300'
+		},
+		colleges: {
+			UMaryland: {
+				name: 'University of Maryland',
+				image:
+					'https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/University_of_Maryland_seal.svg/1200px-University_of_Maryland_seal.svg.png',
+				status: 'Not Submitted'
+			},
+			Cornell: {
+				name: 'Cornell University',
+				image:
+					'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Cornell_University_seal.svg/1200px-Cornell_University_seal.svg.png',
+				status: 'Not Submitted'
+			},
+			CMU: {
+				name: 'Carnegie Mellon University',
+				image:
+					'https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Carnegie_Mellon_University_seal.svg/1200px-Carnegie_Mellon_University_seal.svg.png',
+				status: 'Not Submitted'
+			}
+		},
+		fields: { Name: '', Address: '', Email: '', DOB: '' }
+	}
 
-  createId = () => {
-    fetch("http://52.170.82.100:3000/api/Student")
-      .then(response => {
-        console.log(response.status)
-      })
-      .catch(console.error)
-  }
+	createId = () => {
+		var myHeaders = new Headers()
+		myHeaders.append('Accept', 'application/json')
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+		fetch('http://52.170.82.100:3000/api/Student', {
+			method: 'GET',
+			headers: myHeaders
+		})
+			.then(response => {
+				console.log(response.status)
+				return response
+			})
+			.catch(console.error)
+	}
 
-  profileStyles = {
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "column",
-    justifyContent: "spaceBetween",
-    width: "300px"
-  }
+	handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-  apply = key => {
-    this.props.toggleCollegeView
-    this.setState(prevState => ({
-      ...prevState,
-      colleges: {
-        ...prevState.colleges,
-        UMaryland: {
-          ...prevState.colleges.UMaryland,
-          status: "Pending"
-        }
-      }
-    }))
-  }
+	profileStyles = {
+		display: 'flex',
+		flexWrap: 'wrap',
+		flexDirection: 'column',
+		justifyContent: 'spaceBetween',
+		width: '300px'
+	}
 
-  render() {
-    const { activeItem } = this.state
+	apply = key => {
+		this.props.toggleCollegeView
+		this.setState(prevState => ({
+			...prevState,
+			colleges: {
+				...prevState.colleges,
+				UMaryland: {
+					...prevState.colleges.UMaryland,
+					status: 'Pending'
+				}
+			}
+		}))
+	}
 
-    return (
-      <div>
-        <Grid>
-          <Grid.Column width={this.state.menuItems.length}>
-            <Menu fluid vertical tabular>
-              {this.state.menuItems.map(menuItem => (
-                <Menu.Item
-                  name={menuItem}
-                  active={activeItem === menuItem}
-                  onClick={this.handleItemClick}
-                />
-              ))}
-            </Menu>
-          </Grid.Column>
+	render() {
+		const { activeItem } = this.state
 
-          <Grid.Column stretched width={12}>
-            {activeItem === "Profile" && (
-              <div style={this.profileStyles}>
-                <Header as="h2" icon textAlign="center">
-                  <Icon name="user" circular />
-                  <Header.Content>{this.props.student.name}</Header.Content>
-                  <Header
-                    as="h4"
-                    icon="address book"
-                    content={this.props.student.address}
-                  />
-                  <Header
-                    as="h4"
-                    icon="at"
-                    content={this.props.student.email}
-                  />
-                  <Header
-                    as="h4"
-                    icon="address book"
-                    content={this.props.student.dob}
-                  />
-                </Header>
-              </div>
-            )}
-            {activeItem === "Scores" && (
-              <Card.Group>
-                {Object.entries(this.props.student.scores).map(
-                  ([key, score]) => (
-                    <Card>
-                      <Card.Content>
-                        <Image floated="right" size="small" src={score.image} />
-                        <Card.Header>{score.value}</Card.Header>
-                        <Card.Meta>{score.name}</Card.Meta>
-                        <Card.Description>
-                          Your {score.name} has been posted.
-                        </Card.Description>
-                      </Card.Content>
-                    </Card>
-                  )
-                )}
-              </Card.Group>
-            )}
-            {activeItem === "Colleges" && (
-              <Card.Group>
-                {Object.entries(this.state.colleges).map(([key, college]) => (
-                  <Card>
-                    <Card.Content>
-                      <Image floated="right" size="mini" src={college.image} />
-                      <Card.Header>{college.name}</Card.Header>
-                      <Card.Meta>{college.name}</Card.Meta>
-                      {college.status === "Submitted" && (
-                        <Card.Description>
-                          <p>Your application has been submitted.</p>
-                          <strong>Status: {college.status}</strong>
-                        </Card.Description>
-                      )}
-                    </Card.Content>
-                    <Card.Content extra>
-                      <div>
-                        {college.status === "Not Submitted" && (
-                          <Button color="gray" onClick={key => this.apply()}>
-                            Apply
-                          </Button>
-                        )}
-                        {college.status === "Pending" && (
-                          <Button disabled color="yellow">
-                            Pending
-                          </Button>
-                        )}
-                        {college.status === "Accepted" && (
-                          <Button color="green">Accepted</Button>
-                        )}
-                        {college.status === "Declined" && (
-                          <Button disabled color="red">
-                            Declined
-                          </Button>
-                        )}
-                      </div>
-                    </Card.Content>
-                  </Card>
-                ))}
-              </Card.Group>
-            )}
-          </Grid.Column>
-        </Grid>
-      </div>
-    )
-  }
+		return (
+			<div>
+				<Grid>
+					<Grid.Column width={this.state.menuItems.length}>
+						<Menu fluid vertical tabular>
+							{this.state.menuItems.map(menuItem => (
+								<Menu.Item
+									name={menuItem}
+									active={activeItem === menuItem}
+									onClick={this.handleItemClick}
+								/>
+							))}
+						</Menu>
+					</Grid.Column>
+
+					<Grid.Column stretched width={12}>
+						{activeItem === 'Profile' && (
+							<div style={this.profileStyles}>
+								<Header as="h2" icon textAlign="center">
+									<Icon name="user" circular />
+									<Header.Content>{this.props.student.name}</Header.Content>
+									<Header
+										as="h4"
+										icon="address book"
+										content={this.props.student.address}
+									/>
+									<Header
+										as="h4"
+										icon="at"
+										content={this.props.student.email}
+									/>
+									<Header
+										as="h4"
+										icon="address book"
+										content={this.props.student.dob}
+									/>
+								</Header>
+							</div>
+						)}
+						{activeItem === 'Scores' && (
+							<Card.Group>
+								{Object.entries(this.props.student.scores).map(
+									([key, score]) => (
+										<Card>
+											<Card.Content>
+												<Image floated="right" size="small" src={score.image} />
+												<Card.Header>{score.value}</Card.Header>
+												<Card.Meta>{score.name}</Card.Meta>
+												<Card.Description>
+													Your {score.name} has been posted.
+												</Card.Description>
+											</Card.Content>
+										</Card>
+									)
+								)}
+							</Card.Group>
+						)}
+						{activeItem === 'Colleges' && (
+							<Card.Group>
+								{Object.entries(this.state.colleges).map(([key, college]) => (
+									<Card>
+										<Card.Content>
+											<Image floated="right" size="mini" src={college.image} />
+											<Card.Header>{college.name}</Card.Header>
+											<Card.Meta>{college.name}</Card.Meta>
+											{college.status === 'Submitted' && (
+												<Card.Description>
+													<p>Your application has been submitted.</p>
+													<strong>Status: {college.status}</strong>
+												</Card.Description>
+											)}
+										</Card.Content>
+										<Card.Content extra>
+											<div>
+												{college.status === 'Not Submitted' && (
+													<Button color="gray" onClick={key => this.apply()}>
+														Apply
+													</Button>
+												)}
+												{college.status === 'Pending' && (
+													<Button disabled color="yellow">
+														Pending
+													</Button>
+												)}
+												{college.status === 'Accepted' && (
+													<Button color="green">Accepted</Button>
+												)}
+												{college.status === 'Declined' && (
+													<Button disabled color="red">
+														Declined
+													</Button>
+												)}
+											</div>
+										</Card.Content>
+									</Card>
+								))}
+							</Card.Group>
+						)}
+					</Grid.Column>
+				</Grid>
+			</div>
+		)
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,136 +1,144 @@
-import React, { Component } from "react"
-import { render } from "react-dom"
-import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom"
-import { Segment, Menu } from "semantic-ui-react"
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom'
+import { Segment, Menu } from 'semantic-ui-react'
 
-import Student from "/src/Student"
-import Provider from "/src/Provider"
-import Consumer from "/src/Consumer"
-import Satact from "/src/Satact"
+import Student from '/src/Student'
+import Provider from '/src/Provider'
+import Consumer from '/src/Consumer'
+import Satact from '/src/Satact'
 
 const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center"
+	fontFamily: 'sans-serif',
+	textAlign: 'center'
 }
 
 class App extends Component {
-  state = {
-    activeItem: "",
-    collegeView: true,
-    students: {
-      a: {
-        name: "Satoshi Nakamoto",
-        address: "1600 Pennsylvania Ave. NW",
-        email: "satoshin@gmx.com",
-        dob: "1/3/2009",
-        image: "https://i.ytimg.com/vi/W6g8ShHEXns/maxresdefault.jpg",
-        scores: {
-          GPA: {
-            name: "Bayside High School GPA",
-            image:
-              "https://static.hudl.com/users/temp/3901769_ca93ff3295814d0eb689ed38b754d944.bmp",
-            value: "89.6"
-          },
-          SAT: {
-            name: "SAT Scores",
-            image:
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/New_SAT_Logo_%28vector%29.svg/220px-New_SAT_Logo_%28vector%29.svg.png",
-            value: "1490"
-          }
-        },
-        status: "Pending"
-      }
-    }
-  }
+	state = {
+		activeItem: '',
+		collegeView: true,
+		students: {
+			a: {
+				name: 'Satoshi Nakamoto',
+				address: '1600 Pennsylvania Ave. NW',
+				email: 'satoshin@gmx.com',
+				dob: '1/3/2009',
+				image: 'https://i.ytimg.com/vi/W6g8ShHEXns/maxresdefault.jpg',
+				scores: {
+					GPA: {
+						name: 'Bayside High School GPA',
+						image:
+							'https://static.hudl.com/users/temp/3901769_ca93ff3295814d0eb689ed38b754d944.bmp',
+						value: '89.6'
+					},
+					SAT: {
+						name: 'SAT Scores',
+						image:
+							'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/New_SAT_Logo_%28vector%29.svg/220px-New_SAT_Logo_%28vector%29.svg.png',
+						value: '1490'
+					}
+				},
+				status: 'Pending'
+			}
+		}
+	}
 
-  submitHSScore = newValue => {
-    this.setState(prevState => ({
-      ...prevState,
-      students: {
-        ...prevState.students,
-        a: {
-          ...prevState.students.a,
-          scores: {
-            ...prevState.students.a.GPA,
-            value: newValue
-          }
-        }
-      }
-    }))
-  }
+	submitHSScore = () => {}
 
-  handleItemClick = (e, { name }) => {
-    console.log(this.state)
-    this.setState({
-      activeItem: name
-    })
-  }
+	// submitHSScore = newValue => {
+	//   this.setState(prevState => ({
+	//     ...prevState,
+	//     students: {
+	//       ...prevState.students,
+	//       a: {
+	//         ...prevState.students.a,
+	//         scores: {
+	//           ...prevState.students.a.scores,
+	//           GPA: {
+	//             ...prevState.students.a.scores.GPA,
+	//             value: newValue
+	//           }
+	//         }
+	//       }
+	//     }
+	//   }))
+	// }
 
-  toggleCollegeView = () => {
-    this.setState({ collegeView: !this.state.collegeView })
-  }
+	handleItemClick = (e, { name }) => {
+		console.log(this.state)
+		this.setState({
+			activeItem: name
+		})
+	}
 
-  render() {
-    return (
-      <div style={styles}>
-        <Menu inverted>
-          <Menu.Item
-            name="Student"
-            active={this.state.activeItem === "Student"}
-            onClick={this.handleItemClick}
-            as={Link}
-            to={"/"}
-          />
-          <Menu.Item
-            name="High School"
-            active={this.activeItem === "High School"}
-            onClick={this.handleItemClick}
-            as={Link}
-            to={"/provider"}
-          />
-          <Menu.Item
-            name="SAT/ACT"
-            active={this.activeItem === "SAT/ACT"}
-            onClick={this.handleItemClick}
-            as={Link}
-            to={"/satact"}
-          />
-          <Menu.Item
-            name="University of Maryland"
-            active={this.activeItem === "College"}
-            onClick={this.handleItemClick}
-            as={Link}
-            to={"/consumer"}
-          />
-        </Menu>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={props => (
-              <Student
-                collegeView={this.state.collegeView}
-                toggleCollegeView={this.toggleCollegeView}
-                student={this.state.students.a}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/provider"
-            render={props => <Provider submitScore={this.submitHSScore} />}
-          />
-          <Route path="/consumer" component={Consumer} />
-          <Route path="/satact" component={Satact} />
-        </Switch>
-      </div>
-    )
-  }
+	toggleCollegeView = () => {
+		this.setState({ collegeView: !this.state.collegeView })
+	}
+
+	render() {
+		return (
+			<div style={styles}>
+				<Menu inverted>
+					<Menu.Item
+						name="Student"
+						active={this.state.activeItem === 'Student'}
+						onClick={this.handleItemClick}
+						as={Link}
+						to={'/'}
+					/>
+					<Menu.Item
+						name="High School"
+						active={this.activeItem === 'High School'}
+						onClick={this.handleItemClick}
+						as={Link}
+						to={'/provider'}
+					/>
+					<Menu.Item
+						name="SAT/ACT"
+						active={this.activeItem === 'SAT/ACT'}
+						onClick={this.handleItemClick}
+						as={Link}
+						to={'/satact'}
+					/>
+					<Menu.Item
+						name="University of Maryland"
+						active={this.activeItem === 'College'}
+						onClick={this.handleItemClick}
+						as={Link}
+						to={'/consumer'}
+					/>
+				</Menu>
+				<Switch>
+					<Route
+						exact
+						path="/"
+						render={props => (
+							<Student
+								collegeView={this.state.collegeView}
+								toggleCollegeView={this.toggleCollegeView}
+								student={this.state.students.a}
+							/>
+						)}
+					/>
+					<Route
+						exact
+						path="/provider"
+						render={props => <Provider submitScore={this.submitHSScore} />}
+					/>
+					<Route
+						path="/consumer"
+						render={props => <Consumer students={this.state.students.a} />}
+					/>
+					<Route path="/satact" component={Satact} />
+				</Switch>
+			</div>
+		)
+	}
 }
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
 )
